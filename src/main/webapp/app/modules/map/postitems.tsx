0a1437ce5e86c4196e5bab3d7690d8a9d58b2537
constantin aldea <costinaldea@yahoo.co.uk>
2020-04-05 15:24:14 +0300

    double array

--- src/main/webapp/app/modules/map/postitems.tsx
+++ src/main/webapp/app/modules/map/postitems.tsx
@@ -1,15 +1,15 @@
 import React, {createRef} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import axios from 'axios'
 import {Button} from 'reactstrap';
-import { AvForm, AvField } from 'availity-reactstrap-validation';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {AvForm, AvField} from 'availity-reactstrap-validation';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import Popup from "reactjs-popup";
 import {LatLng} from './map';
 import config from './apiConfig.json';
 
 export interface OwnProps {
-  position: LatLng,
+    position: LatLng,
 };
 type Props = StateProps & DispatchProps & OwnProps
 
@@ -21,35 +21,35 @@ type State = {
     notes: string,
     dailyUse: number
     position: any,
-  };
+};
 
 const form = [
-  {
-    placeholder: "Enter Name", 
-    error: "Required", 
-    required: true, 
-    name: "name", 
-    type: "string", 
-    label: "Name"
-  },
+    {
+        placeholder: "Enter Name",
+        error: "Required",
+        required: true,
+        name: "name",
+        type: "string",
+        label: "Name"
+    },
 
-  {
-    placeholder: "Enter Quantity", 
-    error: "Required", 
-    required: true, 
-    name: "quantity", 
-    type: "number", 
-    label: "Quantity"
-  },
-  
-  {
-    placeholder: "Enter Extra Notes", 
-    error: "Required", 
-    required: true, 
-    name: "notes", 
-    type: "string", 
-    label: "Extra Notes"
-  },
+    {
+        placeholder: "Enter Quantity",
+        error: "Required",
+        required: true,
+        name: "quantity",
+        type: "number",
+        label: "Quantity"
+    },
+
+    {
+        placeholder: "Enter Extra Notes",
+        error: "Required",
+        required: true,
+        name: "notes",
+        type: "string",
+        label: "Extra Notes"
+    },
 //   {
 //     placeholder: "Enter Daily Use", 
 //     error: "Required", 
@@ -72,56 +72,66 @@ const form = [
 // change this accordingly
 
 class PostComponent extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    const {lat, lng} = props.position;
-    this.state = {
-      user: this.props.account,
-      currentStock: 10,
-      dailyUse: 1,
-      name: "",
-      notes: "",
-      position: [
-          lat,
-          lng
-      ],
-      quantity: 2
+    constructor(props) {
+        super(props);
+        const {lat, lng} = props.position;
+        this.state = {
+            user: this.props.account,
+            currentStock: 10,
+            dailyUse: 1,
+            name: "",
+            notes: "",
+            position: [
+                lat,
+                lng
+            ],
+            quantity: 2
+        }
     }
-  }
 
-  postData = (event, errors, values)=> {
-    // post the states in the api
-     axios.post(config.postUri, this.state)
-  }
+    postData = (event, errors, values) => {
+        // post the states in the api
+        const input = {
+            user: this.props.account,
+            currentStock: values["currentStock"],
+            dailyUse: 1,
+            name: values["name"],
+            notes: values["notes"],
+            position: this.state.position,
+            quantity: values["quantity"]
+        };
 
-  render() {
-    const {lat, lng} = this.props.position;
-    return (
-      <div className="post-items-display">
-          <AvForm onSubmit={this.postData}>
-            {form.map(item => (
-                <AvField
-                    key={item.name}
-                    name={item.name}
-                    type={item.type}
-                    label={item.label}
-                    placeholder={item.placeholder}
-                    required={item.required}
-                    errorMessage={item.error}
-                />
-            ))}
-            <Button type="submit"> Submit </Button>
-          </AvForm>
-      </div>
-    )
-  }
+        axios.post(config.postUri, input)
+    }
+
+    render() {
+        const {lat, lng} = this.props.position;
+        return (
+            <div className="post-items-display">
+                <AvForm onSubmit={this.postData}>
+                    {form.map(item => (
+                        <AvField
+                            key={item.name}
+                            name={item.name}
+                            type={item.type}
+                            label={item.label}
+                            placeholder={item.placeholder}
+                            required={item.required}
+                            errorMessage={item.error}
+                        />
+                    ))}
+                    <Button type="submit"> Submit </Button>
+                </AvForm>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = storeState => ({
-  account: storeState.authentication.account,
-  isAuthenticated: storeState.authentication.isAuthenticated
+    account: storeState.authentication.account,
+    isAuthenticated: storeState.authentication.isAuthenticated
 });
-const mapDispatchToProps = { };
+const mapDispatchToProps = {};
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
