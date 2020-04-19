2a486f21d8a95b44eb64d4df59a8b1f5fb608aad
constantin aldea <costinaldea@yahoo.co.uk>
2020-04-19 21:44:15 +0300

    add marker

--- src/main/webapp/app/modules/map/map.tsx
+++ src/main/webapp/app/modules/map/map.tsx
@@ -11,6 +11,7 @@ import LeftPanel from './leftpanel';
 import {geolocated} from "react-geolocated";
 import 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import Any = jasmine.Any;
 
 declare global {
   interface Window {
@@ -57,147 +58,162 @@ const types = {
 };
 let position = [51.505, -0.09];
 
-  class MapComponent extends React.Component<MapProps, State> {
-    private resourceSuppliersMap: Map;
-    circle = {};
-    state = {
+let currentMarker = undefined;
+
+class MapComponent extends React.Component<MapProps, State> {
+  private resourceSuppliersMap: Map;
+  circle = {};
+  state = {
+    open: false,
+    latlng: defaultLatLng,
+    type: types.Get,
+    radius: 10
+  };
+
+  changeRadius = (event, error, values) => {
+    this.setState({
+      radius: values.radius
+    })
+  }
+
+  closeModal = () => {
+    this.setState({
       open: false,
       latlng: defaultLatLng,
       type: types.Get,
-      radius: 10
-    };
+    })
+  }
 
-    changeRadius = (event, error, values) => {
-      this.setState({
-        radius: values.radius
-      })
-    }
-
-    closeModal = () => {
-      this.setState({
-        open: false,
-        latlng: defaultLatLng,
-        type: types.Get,
-      })
-    }
-
-    componentDidMount() {
-      // use this.props.account to set view after the geo location of user is obtained
+  componentDidMount() {
+    // use this.props.account to set view after the geo location of user is obtained
 
 
 // check for Geolocation support
-      if (window.navigator.geolocation) {
-        window.navigator.geolocation.getCurrentPosition((pos) => {
-          position = [pos.coords.latitude, pos.coords.longitude]
-
-          this.resourceSuppliersMap = L.map('map-container').setView(position, 10);
-          this.resourceSuppliersMap.on('click', (event) => this.onMapClicked(event));
-          this.setTitleLayer();
-
-        }, (error) => {
-          alert("Problem getting geolocation " + error.message);
-          /**
-           * Add a different strategy for identifying users
-           */
-          position = [this.state.latlng.lat, this.state.latlng.lng]
-
-          this.resourceSuppliersMap = L.map('map-container').setView(position, 10);
-          this.resourceSuppliersMap.on('click', (event) => this.onMapClicked(event));
-          this.setTitleLayer();
-        })
-      } else {
-        alert('Geolocation is not supported for this Browser/OS.');
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition((pos) => {
+        position = [pos.coords.latitude, pos.coords.longitude]
+        this.extracted(position);
+      }, (error) => {
+        alert("Problem getting geolocation " + error.message);
+        /**
+         * Add a different strategy for identifying users
+         */
         position = [this.state.latlng.lat, this.state.latlng.lng]
 
         this.resourceSuppliersMap = L.map('map-container').setView(position, 10);
         this.resourceSuppliersMap.on('click', (event) => this.onMapClicked(event));
         this.setTitleLayer();
-      }
-    }
 
-    onButtonClicked = (latlng, type, event) => {
-      this.setState({
-        open: true,
-        type,
       })
-    }
+    } else {
+      alert('Geolocation is not supported for this Browser/OS.');
+      position = [this.state.latlng.lat, this.state.latlng.lng]
 
-    removeCircle = () => {
-      this.resourceSuppliersMap.removeLayer(this.circle)
-    }
-
-    onMapClicked = (event) => {
-      this.removeCircle()
-
-      L.popup()
-        .setLatLng(event.latlng)
-        .setContent((layer) => this.showPopup(layer, event.latlng))
-        .openOn(this.resourceSuppliersMap)
-        .on('remove', () => {
-          this.removeCircle();
-        })
-
-      this.circle = L.circle(event.latlng, this.state.radius * 1000).addTo(this.resourceSuppliersMap);
-      this.setState({
-        latlng: event.latlng
-      });
-    }
-
-    showPopup = (layer, latlng) => {
-      const node = L.DomUtil.create('div', 'popup-div info-div');
-      Object.keys(types).forEach(type => {
-        const button = L.DomUtil.create('button', 'popup-button btn btn-secondary', node);
-        button.innerHTML = type;
-        button.onclick = (e) => this.onButtonClicked(latlng, type, e);
-      });
-      return node;
-    }
-
-    setTitleLayer() {
-      const baseTileString = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-      const options = {
-        attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
-      };
-
-      L.tileLayer(baseTileString, options).addTo(this.resourceSuppliersMap);
-    }
-
-
-    render() {
-      return (
-        <Row>
-          <Col md="3">
-            <LeftPanel radius={this.state.radius} position={this.state.latlng} changeRadius={this.changeRadius}/>
-          </Col>
-          <Col md="9">
-            <div>
-              <div id='map-container'></div>
-              <Popup
-                open={this.state.open}
-                closeOnDocumentClick
-                onClose={this.closeModal}
-              >
-                {
-                  this.state.type === types.Get
-                    ? <GetItems position={this.state.latlng} radius={this.state.radius}/>
-                    : <PostItems position={this.state.latlng}/>
-                }
-              </Popup>
-            </div>
-          </Col>
-        </Row>
-      )
+      this.resourceSuppliersMap = L.map('map-container').setView(position, 10);
+      this.resourceSuppliersMap.on('click', (event) => this.onMapClicked(event));
+      this.setTitleLayer();
     }
   }
 
-  const mapStateToProps = storeState => ({
-    account: storeState.authentication.account,
-    isAuthenticated: storeState.authentication.isAuthenticated
-  });
+  private extracted(pos) {
+    this.resourceSuppliersMap = L.map('map-container').setView(pos, 10);
+    this.resourceSuppliersMap.on('click', (event) => this.onMapClicked(event));
+    this.setTitleLayer();
+    const browserLatLng = {
+      lat: position[0],
+      lng: position[1],
+    };
 
-  const mapDispatchToProps = {};
+    currentMarker = new L.Marker(browserLatLng).addTo(this.resourceSuppliersMap);
+  }
 
-  type StateProps = ReturnType<typeof mapStateToProps>;
-  type DispatchProps = typeof mapDispatchToProps;
+  onButtonClicked = (latlng, type, event) => {
+    position = [latlng.lang, latlng.lng]
+    this.setState({
+      open: true,
+      type,
+    })
+  }
 
-  export default connect(mapStateToProps)(MapComponent);
+  removeCircle = () => {
+    this.resourceSuppliersMap.removeLayer(this.circle)
+    this.resourceSuppliersMap.removeLayer(currentMarker)
+  }
+
+  onMapClicked = (event) => {
+    this.removeCircle()
+    position = [event.latlng.lat, event.latlng.lng]
+
+    L.popup()
+      .setLatLng(event.latlng)
+      .setContent((layer) => this.showPopup(layer, event.latlng))
+      .openOn(this.resourceSuppliersMap)
+      .on('remove', () => {
+        this.removeCircle();
+      })
+
+    this.circle = L.circle(event.latlng, this.state.radius * 1000).addTo(this.resourceSuppliersMap);
+    this.setState({
+      latlng: event.latlng
+    });
+    currentMarker = new L.Marker(event.latlng).addTo(this.resourceSuppliersMap);
+  }
+
+  showPopup = (layer, latlng) => {
+    const node = L.DomUtil.create('div', 'popup-div info-div');
+    Object.keys(types).forEach(type => {
+      const button = L.DomUtil.create('button', 'popup-button btn btn-secondary', node);
+      button.innerHTML = type;
+      button.onclick = (e) => this.onButtonClicked(latlng, type, e);
+    });
+    return node;
+  }
+
+  setTitleLayer() {
+    const baseTileString = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const options = {
+      attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
+    };
+
+    L.tileLayer(baseTileString, options).addTo(this.resourceSuppliersMap);
+  }
+
+
+  render() {
+    return (
+      <Row>
+        <Col md="3">
+          <LeftPanel radius={this.state.radius} position={this.state.latlng} changeRadius={this.changeRadius}/>
+        </Col>
+        <Col md="9">
+          <div>
+            <div id='map-container'></div>
+            <Popup
+              open={this.state.open}
+              closeOnDocumentClick
+              onClose={this.closeModal}
+            >
+              {
+                this.state.type === types.Get
+                  ? <GetItems position={this.state.latlng} radius={this.state.radius}/>
+                  : <PostItems position={this.state.latlng}/>
+              }
+            </Popup>
+          </div>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+const mapStateToProps = storeState => ({
+  account: storeState.authentication.account,
+  isAuthenticated: storeState.authentication.isAuthenticated
+});
+
+const mapDispatchToProps = {};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps)(MapComponent);
