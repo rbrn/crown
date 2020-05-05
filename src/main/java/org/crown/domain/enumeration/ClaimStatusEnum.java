5292d20c0bd018d57dd4f7ec66314057370bc35e
constantin aldea <costinaldea@yahoo.co.uk>
2020-05-05 09:58:07 +0300

    adding status

---
+++ src/main/java/org/crown/domain/enumeration/ClaimStatusEnum.java
@@ -0,0 +1,8 @@
+package org.crown.domain.enumeration;
+
+/**
+ * The ClaimStatusEnum enumeration.
+ */
+public enum ClaimStatusEnum {
+    PENDING, INREVIEW, ACCEPTED, FORSHIPPING, SHIPPED, DELIVERED
+}
