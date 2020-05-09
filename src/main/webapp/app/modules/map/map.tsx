854510e4473d8910da3ff6b81f58d87b419dcd7c
Ani-Utkin <60753926+Ani-Utkin@users.noreply.github.com>
2020-05-09 09:30:11 -0400

    updated map layout

--- src/main/webapp/app/modules/map/map.tsx
+++ src/main/webapp/app/modules/map/map.tsx
@@ -89,10 +89,6 @@ let position = [51.505, -0.09];
 
 let currentMarker = undefined;
 
-//const map = L.map('map-container').setView([51.505, -0.09], 13);
-
-//const pane = map.createPane('fixed', document.getElementById('map-container'));
-
 
 class MapComponent extends React.Component<MapProps, State> {
   private resourceSuppliersMap: Map;
@@ -286,13 +282,13 @@ class MapComponent extends React.Component<MapProps, State> {
     }
 
     return (
-      <Container>
-      <Row>
-        <Col md="3">
+      <Container className="myclass col-auto ml-auto">
+        <Row>
+        <Col className="col-sm-3 p-0">
           <LeftPanel radius={this.state.radius} position={this.state.latlng} changeRadius={this.changeRadius}/>
         </Col>
-        <Col md="9">
-          <div className="shadow-lg p-3 mb-5 bg-white rounded">
+        <Col className="col-lg-9 p-0">
+          <div className="rounded">
             <div id='map-container'></div>
             <Popup
               open={this.state.open}
@@ -303,7 +299,6 @@ class MapComponent extends React.Component<MapProps, State> {
                   ? <PostedItemsComponent position={this.state.latlng} radius={this.state.radius}/>
                   : <RequestedItemsComponent position={this.state.latlng} radius={this.state.radius}/>
               }
-
             </Popup>
           </div>
         </Col>
