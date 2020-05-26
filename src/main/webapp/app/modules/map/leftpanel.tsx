ac1bb3fc52fab89d1511cdb3b810f8e58787d4d7
Ani-Utkin <60753926+Ani-Utkin@users.noreply.github.com>
2020-05-26 15:05:29 -0400

    Update leftpanel.tsx

--- src/main/webapp/app/modules/map/leftpanel.tsx
+++ src/main/webapp/app/modules/map/leftpanel.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {LatLng} from './map';
-import {Button} from 'reactstrap';
+import {Button, Card} from 'reactstrap';
 import { AvField, AvForm } from 'availity-reactstrap-validation';
 
 
