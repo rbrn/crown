ffdc727c056dc08cf20e74374d842301fd333d22
Ani-Utkin <60753926+Ani-Utkin@users.noreply.github.com>
2020-06-19 20:45:14 -0400

    claim update

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -46,8 +46,8 @@ module.exports = options =>
       proxy: [
         {
           context: ['/api', '/services', '/management', '/swagger-resources', '/v2/api-docs', '/h2-console', '/auth'],
-          target: `http${options.tls ? 's' : ''}://crown-dev.herokuapp.com`,
-          secure: true,
+          target: `http${options.tls ? 's' : ''}://localhost:8080`,
+          secure: false,
           changeOrigin: options.tls
         }
       ],
