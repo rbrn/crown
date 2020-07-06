bc677106a7383a35253277bf90836d52a777ae7b
Madhvi Sharma <madhvi.sharma66@gmail.com>
2020-07-05 22:00:23 -0400

    Add new fields to Request/Supply pages

--- src/main/webapp/app/shared/model/supplier-resource.model.ts
+++ src/main/webapp/app/shared/model/supplier-resource.model.ts
@@ -1,10 +1,15 @@
+import { Moment } from 'moment';
 import { IResourceType } from 'app/shared/model/resource-type.model';
 import { IReceiverSupplier } from 'app/shared/model/receiver-supplier.model';
 
 export interface ISupplierResource {
   id?: string;
   quantity?: number;
+  quantityValidUntil?: Moment;
   cost?: number;
+  productAvailabilityLeadTime?: number;
+  minOrderQuantity?: number;
+  quantityOnHand?: number;
   resourceType?: IResourceType;
   supplier?: IReceiverSupplier;
 }
